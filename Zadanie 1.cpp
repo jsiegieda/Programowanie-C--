6904fe9f880d9e8aee72a3135ec586c0478f0d44
jsiegieda <jsiegieda555@gmail.com>
2018-05-03 23:30:41 +0200

    Dodane zadanie 1

---
+++ Zadanie 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+class poczta
+{
+    public:
+    string nadawca;
+    string odbiorca;
+    string temat;
+    string tresc;
+
+};
+
+void wypisz(poczta x){
+    cout<<"Nadawca : "<<endl;
+    cin>>x.nadawca;
+    cout<<"Odbiorca : "<<endl;
+    cin>>x.odbiorca;
+    cout<<"Temat : "<<endl;
+    cin>>x.temat;
+    cout<<"Tresc : "<<endl;
+    cin>>x.tresc;
+}
+
+int main()
+{
+poczta x;
+
+wypisz(x);
+
+    return 0;
+}
